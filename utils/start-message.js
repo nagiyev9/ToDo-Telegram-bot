144b8831bbecbf3ea1ee14c58f3a7ee2fd834ca0
Nagiyev Mahaddin <meheddinngyv9@gmail.com>
2024-10-09 17:29:55 +0400

    Updated bot code

--- utils/start-message.js
+++ utils/start-message.js
@@ -2,7 +2,7 @@ const sendStartMessage = async (ctx) => {
     const helperCommand = `<b>Welcome to Just Do It Bot. I'm here to help you manage your tasks effectively with this To-Do Bot. Whether it's simple daily tasks or long-term goals, you can create, update, and track your to-dos easily. Let's get started on organizing your tasks!</b>\n
 <b>/start</b> - <i>Start the bot</i>\n
 <b>/help</b> - <i>Command list</i>\n
-<b>/set</b> - <i>Set New Task</i>`;
+<b>/set</b> - <i>Create New Task</i>`;
 
     try {
         await ctx.reply(helperCommand, {
